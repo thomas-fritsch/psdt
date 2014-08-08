7c678a121fad37935ec082ee60c0c1f3824f97b5
Thomas Fritsch <thomas.fritsch@oce.com>
2014-08-08 20:48:09 +0200

    code more readable

--- de.tfritsch.psdt.ui/src/de/tfritsch/psdt/ui/hover/PostscriptEObjectDocumentationProvider.java
+++ de.tfritsch.psdt.ui/src/de/tfritsch/psdt/ui/hover/PostscriptEObjectDocumentationProvider.java
@@ -1,45 +1,45 @@
 package de.tfritsch.psdt.ui.hover;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.help.HelpSystem;
 import org.eclipse.help.IContext;
 import org.eclipse.help.IHelpResource;
+import org.eclipse.jface.internal.text.html.HTMLPrinter;
 import org.eclipse.xtext.documentation.IEObjectDocumentationProvider;
 
 import de.tfritsch.psdt.postscript.PSExecutableName;
 
+@SuppressWarnings("restriction")
 public class PostscriptEObjectDocumentationProvider implements
 		IEObjectDocumentationProvider {
 
 	public String getDocumentation(EObject o) {
 		if (o instanceof PSExecutableName) {
-			IContext context = HelpSystem
-					.getContext("de.tfritsch.psdt.help.Reference");
-			if (context == null)
-				return null;
-			String href = getHref(context, ((PSExecutableName) o).getName());
-			if (href == null)
-				return null;
-			InputStream input = HelpSystem.getHelpContent(href);
-			if (input == null)
-				return null;
-			int posHash = href.indexOf('#');
-			String fragment = (posHash >= 0) ? href.substring(posHash + 1) : "";
-			String full = "";
-			try {
-				full = toString(input);
-				input.close();
-			} catch (IOException e) {
-			}
-			return getBetween(full, "<A NAME=\"" + fragment + "\"></A>", "<HR>");
+			return doGetDocumentation((PSExecutableName) o);
 		}
 		return null;
 	}
 
-	private String getHref(IContext context, String label) {
+	protected String doGetDocumentation(PSExecutableName o) {
+		String href = getHref("de.tfritsch.psdt.help.Reference", o.getName());
+		if (href == null)
+			return null;
+		String content = getContent(href);
+		if (content == null)
+			return null;
+		int posHash = href.indexOf('#');
+		String fragment = (posHash >= 0) ? href.substring(posHash + 1) : "";
+		return getBetween(content, "<A NAME=\"" + fragment + "\"></A>", "<HR>");
+	}
+
+	private String getHref(String contextId, String label) {
+		IContext context = HelpSystem.getContext(contextId);
+		if (context == null)
+			return null;
 		IHelpResource[] topics = context.getRelatedTopics();
 		for (IHelpResource topic : topics) {
 			if (topic.getLabel().equals(label)) {
@@ -49,14 +49,16 @@ public class PostscriptEObjectDocumentationProvider implements
 		return null;
 	}
 
-	private String toString(InputStream input) throws IOException {
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-		int nRead;
-		byte[] data = new byte[16384];
-		while ((nRead = input.read(data)) != -1) {
-			buffer.write(data, 0, nRead);
+	private String getContent(String href) {
+		InputStream input = HelpSystem.getHelpContent(href);
+		if (input == null)
+			return null;
+		String content = HTMLPrinter.read(new InputStreamReader(input));
+		try {
+			input.close();
+		} catch (IOException e) {
 		}
-		return buffer.toString();
+		return content;
 	}
 
 	private String getBetween(String full, String begin, String end) {
