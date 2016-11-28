1a12267db21b66d2bd8cd9cf94239ea4a869f45a
Thomas Fritsch <thomas.fritsch@oce.com>
2016-11-28 22:07:24 +0100

    Improved shell-script for deploying to Bintray

--- deployToBintray.sh
+++ deployToBintray.sh
@@ -16,10 +16,13 @@ BT_REPO=$2
 BT_PACKAGE=$3
 BT_VERSION=$4
 
+echo "creating version $BT_VERSION ..."
+curl -X POST -u $BINTRAY_USER:$BINTRAY_API_KEY -H "Content-Type: application/json" -d "{\"name\":\"$BT_VERSION\",\"desc\":\"Created by Travis CI\"}" "https://api.bintray.com/packages/$BT_OWNER/$BT_REPO/$BT_PACKAGE/versions"
+echo
 cd $LOCAL_PATH
 for F in artifacts.* content.* plugins/* features/*
 do
-  echo deploying $F ...
-  curl -X PUT -T $F -u $BINTRAY_USER:$BINTRAY_API_KEY "https://api.bintray.com/content/$BT_OWNER/$BT_REPO/$BT_PACKAGE/$BT_VERSION/$F;bt_package=$BT_PACKAGE;bt_version=$BT_VERSION"
+  echo "deploying $F ..."
+  curl -X PUT -T $F -u $BINTRAY_USER:$BINTRAY_API_KEY "https://api.bintray.com/content/$BT_OWNER/$BT_REPO/$BT_PACKAGE/$BT_VERSION/$F;bt_package=$BT_PACKAGE;bt_version=$BT_VERSION;publish=1"
   echo
 done
